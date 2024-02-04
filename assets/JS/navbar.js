f6537bb8a1d7801a5d376032186275dd937a1cef
Aldwin <elonyx7@gmail.com>
2024-02-04 15:46:27 +0100

    test navbar.js to fix

---
+++ assets/JS/navbar.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("Nav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("Nav").style.height = "0%";
+}
--- view/accueil.php
+++ view/accueil.php
@@ -5,11 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Portfolio ━━ Aldwin WEBER</title>
         <link rel="icon" type="image/x-icon" href="https://images.vexels.com/media/users/3/205472/isolated/preview/62c3ddcae4b835271eb633cb80c8c797-planet-mars-illustration.png" />
-        <link rel="stylesheet" href="/CSS/styles.css" />
-        <link rel="stylesheet" href="/scss/navbar.scss">
+        <link rel="stylesheet" href="assets/CSS/styles.css" />
+
     </head>
     <body>
         <?php require_once 'view/components/navbar.html' ?>
+        <?php require_once 'assets/JS/navbar.js'?>
     </body>
-    <script type="text/javascript" src="/JS/navbar.js"></script>
+       
 </html>
