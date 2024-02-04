3e879699dce009c595a7859b38e3218718946e35
Aldwin <elonyx7@gmail.com>
2024-02-04 14:15:51 +0100

    change files and folder to mvc

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+if (isset($_GET['page'])){  
+  switch($_GET['page']){
+      // index.php?page=accueil
+      case 'accueil':
+          require_once 'controller/accueil.php';
+          break;
+
+
+
+
+
+
+
+
+
+          
+      
+      }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
---
+++ view/accueil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Portfolio ━━ Aldwin WEBER</title>
+        <link rel="icon" type="image/x-icon" href="https://images.vexels.com/media/users/3/205472/isolated/preview/62c3ddcae4b835271eb633cb80c8c797-planet-mars-illustration.png" />
+        <link rel="stylesheet" href="/CSS/styles.css" />
+        <link rel="stylesheet" href="/scss/navbar.scss">
+    </head>
+    <body>
+        <?php require_once 'view/components/navbar.html' ?>
+    </body>
+    <script type="text/javascript" src="/JS/navbar.js"></script>
+</html>
