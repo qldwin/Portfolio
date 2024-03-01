8a24ada59f78b92ae265abeeb955a5259c0262f9
Aldwin <elonyx7@gmail.com>
2024-03-01 17:14:31 +0100

    add strings and loop in typing.js

--- assets/JS/typing.js
+++ assets/JS/typing.js
@@ -1,4 +1,5 @@
 var typed = new Typed("#element", {
-    strings: ["Developpeur WEB.", "Etudiant en BTS SIO."],
-    typeSpeed: 50,
+    strings: ["Développeur WEB.", "Étudiant en BTS SIO.", "J'ai 20 ans.", "J'habite au Mans", "J'étudie à Nîmes.", "Lycée CCI GARD."],
+    typeSpeed: 100,
+    loop:true
 });
