8bd6506bc5c0f4a0c2a86242d5ec018436fd4e27
Aldwin <elonyx7@gmail.com>
2024-03-12 11:36:33 +0100

    fix projects part and add bubbles.js

--- assets/JS/slider.js
+++ assets/JS/slider.js
@@ -1,49 +1,41 @@
 const slides = document.querySelectorAll(".projet_item#slide");
 let currentSlideIndex = 0;
 
-// Affiche la première diapositive
-slides[currentSlideIndex].classList.add('active');
+slides[currentSlideIndex].classList.add("active");
 
 function ChangeSlide(sens) {
-    // Cacher la diapositive actuelle
-    slides[currentSlideIndex].classList.remove('active');
+    slides[currentSlideIndex].classList.remove("active", "previous", "next");
 
-    // Calculer l'index de la prochaine diapositive
     let nextSlideIndex = currentSlideIndex + sens;
+    let previousSlideIndex = (currentSlideIndex - sens + slides.length) % slides.length;
 
-    // Gérer les limites
     if (nextSlideIndex < 0) {
         nextSlideIndex = slides.length - 1;
     } else if (nextSlideIndex >= slides.length) {
         nextSlideIndex = 0;
     }
 
-    // Afficher la nouvelle diapositive
-    slides[nextSlideIndex].classList.add('active');
+    slides[nextSlideIndex].classList.add("active");
 
-    // Mettre à jour l'index de la diapositive actuelle
     currentSlideIndex = nextSlideIndex;
 
-    // Flouter les diapositives précédente et suivante
-    const previousSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    const previousSlideIndex2 = (currentSlideIndex - 1 + slides.length) % slides.length;
     const nextSlideIndex2 = (currentSlideIndex + 1) % slides.length;
 
-    slides[previousSlideIndex].classList.add('previous');
-    slides[nextSlideIndex2].classList.add('next');
+    slides[previousSlideIndex2].classList.add("previous");
+    slides[nextSlideIndex2].classList.add("next");
 }
 
 let timerId;
 
 function startCountdown() {
-    clearTimeout(timerId); // Réinitialisez le timer si l'utilisateur survole à nouveau la div
+    clearTimeout(timerId);
 
-    // Affichez les boutons
-    document.getElementById('precedent').style.opacity = 1;
-    document.getElementById('suivant').style.opacity = 1;
+    document.getElementById("precedent").style.opacity = 1;
+    document.getElementById("suivant").style.opacity = 1;
 
-    // Démarrez le compte à rebours de 3 secondes (3000 ms)
-    timerId = setTimeout(function() {
-        document.getElementById('precedent').style.opacity = 0;
-        document.getElementById('suivant').style.opacity = 0;
-    }, 3000); // 3000 ms = 3 secondes
+    timerId = setTimeout(function () {
+        document.getElementById("precedent").style.opacity = 0;
+        document.getElementById("suivant").style.opacity = 0;
+    }, 5000);
 }
--- view/accueil.php
+++ view/accueil.php
@@ -19,6 +19,27 @@
     </header>
     <div class="blur-container"></div>
     <section id="accueil" class="section-body" href="#accueil">
+        <div class="Box_Python">
+            <img class="logo_Python" src="/assets/img/Python-logo.png">
+        </div>
+        <div class="Box_JS">
+            <img class="logo_JS" src="/assets/img/js-logo.png">
+        </div>
+        <div class="Box_CSS">
+            <img class="logo_CSS" src="/assets/img/css3-logo.png">
+        </div>
+        <div class="Box_HTML">
+            <img class="logo_HTML" src="/assets/img/html-logo.png">
+        </div>
+        <div class="Box_JAVA">
+            <img class="logo_JAVA" src="/assets/img/Java-logo.png">
+        </div>
+        <div class="Box_CSHARP">
+            <img class="logo_CSHARP" src="/assets/img/csharp-logo.png">
+        </div>
+        <div class="Box_SQLServer">
+            <img class="logo_SQLServer" src="/assets/img/SQLServer-logo.png">
+        </div>
         <div class="div-infos">
             <h4 class="me-infos">Je suis</h4>
             <h1 class="title-center">WEBER ALDWIN</h1>
@@ -33,7 +54,7 @@
         VEILLE
     </section>
     <section id="contact" class="section-body" href="#contact">
-        CONTACT 
+        CONTACT
     </section>
 
     <footer>
