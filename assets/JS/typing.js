7ea7f320c2b136189b200188ea0dd883418fc1a0
Aldwin <elonyx7@gmail.com>
2024-07-22 13:57:10 +0200

    delete bubble language, replace black png to white png, fix arrow align in project

--- assets/JS/typing.js
+++ assets/JS/typing.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#element", {
-    strings: ["Développeur WEB.", "Étudiant en BTS SIO.", "J'ai 20 ans.", "J'habite au Mans", "J'étudie à Nîmes.", "Lycée CCI GARD."],
-    typeSpeed: 100,
+    strings: ["Développeur Full-Stack.", "Étudiant en BTS SIO.", "J'ai 20 ans.", "J'habite au Mans.", "J'étudie à Nîmes.", "Lycée CCI GARD."],
+    typeSpeed: 50,
     loop:true
 });
