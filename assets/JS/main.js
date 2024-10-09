3bc3329cee10d69b174ec29a759c7c9a0d55a2c4
Aldwin <elonyx7@gmail.com>
2024-10-09 19:04:07 +0200

    New branch

--- assets/JS/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Défilez jusqu'à la section #accueil
-    document.getElementById('accueil').scrollIntoView({ behavior: 'smooth' });
-});
