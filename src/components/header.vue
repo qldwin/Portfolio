b582192e8fa0a6df4a4a6e4370fabd871015bc67
Aldwin <elonyx7@gmail.com>
2024-11-05 19:25:17 +0100

    fix align navbar

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,8 +1,10 @@
 <template>
     <header>
         <nav class="navbar">  
-            <ul class="list-navbar">
+            <ul class="pic-navbar-list">
                 <li class="pic-navbar-div"><img class="pic-navbar" src="/public/moi.webp" alt="portrait de Aldwin WEBER" /></li>
+            </ul>
+            <ul class="list-navbar">
                 <li class="content-navbar"><a aria-current="page" href="#accueil">Accueil</a></li>
                 <li class="content-navbar"><a aria-current="page" href="#projets">Projets</a></li>
                 <li class="content-navbar"><a aria-current="page" href="#competences">Compétences</a></li>
