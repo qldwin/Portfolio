d5d5c18f9944fff2bb2c340e2ee5830a50845b72
nathan <nathan.sunyt@gmail.com>
2024-12-18 19:26:40 +0100

    fix: move import for css

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Accueil from "./components/accueil.vue";
 import Competences from "./components/competences.vue";
 import Projets from "./components/projects.vue";
 import Contact from "./components/contact.vue";
+import "./assets/CSS/style.css";
 </script>
 
 <template>
