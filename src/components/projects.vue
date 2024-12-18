689e0666d82813f875bb4b6ff61f3744475bf5e7
nathan <nathan.sunyt@gmail.com>
2024-12-18 19:17:56 +0100

    fix css for projects

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -38,4 +38,66 @@ export default {
 };
 </script>
 
-<style src="../assets/css/projects.css"></style>
+<style scoped>
+.projets-carte-visite {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  padding: 40px;
+  justify-items: center;
+}
+
+.carte {
+  background-color: #333;
+  border-radius: 12px;
+  padding: 20px;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  width: 100%;
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.carte:hover {
+  transform: scale(1.05);
+  box-shadow: 0 12px 24px rgba(0, 0, 0, 0.5);
+}
+
+.carte-header h2 {
+  color: #cccccc;
+  font-size: 22px;
+  margin-bottom: 10px;
+}
+
+.projet_languages {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.projet_languages span {
+  background-color: #444;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin-right: 8px;
+  margin-bottom: 5px;
+  color: #cccccc;
+  font-size: 14px;
+}
+
+.carte-body p {
+  color: #cccccc;
+
+  font-size: 14px;
+  line-height: 1.5;
+  margin-top: 10px;
+}
+
+@media (max-width: 768px) {
+  .projets-carte-visite {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
+}
+
+</style>
