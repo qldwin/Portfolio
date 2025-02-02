6fd4a3056743dfd50f379d1bf22a43086520a2a4
Aldwin <elonyx7@gmail.com>
2025-02-02 18:03:13 +0100

    delete scroll.js (useless), fix description and fix contact section

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,14 +7,15 @@
     <div class="description">
       <p>
                       Je suis passionné par le développement et toujours curieux d’apprendre de nouvelles technologies. <br>
-        Mon parcours, BAC STI2D à mon BTS SIO SLAM, m’a permis d’explorer pas mal d’aspects du développement, que ce soit côté front ou back. <br>
+        Mon parcours, de mon BAC STI2D à mon BTS SIO SLAM, m’a permis d’explorer pas mal d’aspects du développement, que ce soit côté front ou back. <br>
         J’aime coder des petits projets qui sont à la fois efficaces et agréables à utiliser, en cherchant toujours à optimiser les performances et l’ergonomie. <br>
         <br>
         Sur ce portfolio, vous trouverez quelques-uns de mes projets, où j’ai pu mettre en pratique mes compétences en développement FullStack. <br>
-        Je suis toujours partant pour échanger, partager des idées ou relever de nouveaux défis, alors n’hésite pas à me contacter ! 🚀
+        Je suis toujours partant pour échanger, partager des idées ou relever de nouveaux défis, alors n’hésitez pas à me contacter ! 🚀
       </p>
     </div>
     <div class="div-social-media">
+      <a href="/src/assets/img/CV.pdf" download class="download-link">Télécharger mon CV</a>
       <div class="social-media">
         <a href="https://github.com/qldwin" target="_blank">
           <img class="logo-social-media" src="/src/assets/img/Github-logo.webp" alt="logo GitHub"/>
