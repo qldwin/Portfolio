5a40fece6fded54241e17d3feaa4e93036d3a670
qldwin <elonyx7@gmail.com>
2025-02-04 12:40:12 +0100

    change design contact

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -3,23 +3,14 @@
     <div class="contacts-container">
       <div class="Box_Contacts">
         <h1>Contact</h1>
-
         <h4>Pour me contacter, envoyez-moi un mail ci-dessous.</h4>
         <form action="" method="post">
-          <input class="name" type="text" name="name" aria-label="expediteur nom" placeholder="Jean DUPONT"/>
-          <input class="email" type="email" name="email" aria-label="expediteur mail"
-                 placeholder="jean.dupont@gmail.com"/>
-          <textarea name="message" class="contactText" placeholder="Détail de votre demande"></textarea>
-          <input class="submit" aria-label="expediteur message" type="submit" value="Envoyez un message"/>
+          <input class="text-input" type="text" name="name" aria-label="expediteur nom" placeholder="Jean DUPONT"/>
+          <input class="text-input" type="email" name="email" aria-label="expediteur mail" placeholder="jean.dupont@gmail.com"/>
+          <textarea name="text-input" class="contactText" placeholder="Détail de votre demande"></textarea>
+          <button class="submit" aria-label="expediteur message" type="submit" value="Envoyez un message">Envoyer le mail</button>
         </form>
       </div>
-
-      <nav class="social-navbar">
-        <ul>
-          <li class="social-li-navbar"></li>
-          <li class="social-li-navbar"></li>
-        </ul>
-      </nav>
     </div>
   </section>
 </template>
