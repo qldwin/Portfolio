042b1e83ff404ebf44e8415e96610a2b4d0ed0ac
qldwin <elonyx7@mail.com>
2025-02-04 19:58:54 +0100

    fix languages card and add tools card in skills section

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="skills" class="section-body">
-    <grid>
+    <grid class="grid-about">
       <div class="card-languages">
-        <h1>Langages</h1>
+        <h1 class="title-languages">Langages et Framework</h1>
         <grid class="grid-languages">
           <span class="skills_languages html">HTML</span>
           <span class="skills_languages css">CSS</span>
@@ -12,14 +12,26 @@
           <span class="skills_languages php">PHP</span>
           <span class="skills_languages java">Java</span>
           <span class="skills_languages csharp">C#</span>
-          <span class="skills_languages visualbasic">Visual Basic</span>
+          <span class="skills_languages visualbasic">VisualBasic</span>
           <span class="skills_languages flutter">Flutter</span>
           <span class="skills_languages cpp">C++</span>
         </grid>
-
       </div>
-      <div class="div-tools">
-
+      <div class="card-tools">
+        <h1 class="title-tools">Outils</h1>
+        <grid class="grid-tools">
+          <span class="skills_tools linux">Linux</span>
+          <span class="skills_tools github">GitHub</span>
+          <span class="skills_tools git">Git</span>
+          <span class="skills_tools docker">Docker</span>
+          <span class="skills_tools visualstudio">VisualStudio</span>
+          <span class="skills_tools webstorm">Webstorm</span>
+          <span class="skills_tools phpstorm">PhpStorm</span>
+          <span class="skills_tools sqlserver">SqlServer</span>
+          <span class="skills_tools mysql">MySql</span>
+          <span class="skills_tools jetbrains">Jetbrains</span>
+          <span class="skills_tools vscode">VScode</span>
+        </grid>
       </div>
     </grid>
   </section>
