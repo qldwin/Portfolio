dfaf73122cf4f5983f9f17ee797855ad7426f5a5
qldwin <elonyx7@mail.com>
2025-02-05 21:05:59 +0100

    change skills cards with black abd white img, delete useless code

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -4,33 +4,33 @@
       <div class="card-languages">
         <h1 class="title-languages">Langages et Framework</h1>
         <grid class="grid-languages">
-          <span class="skills_languages html">HTML</span>
-          <span class="skills_languages css">CSS</span>
-          <span class="skills_languages javascript">JavaScript</span>
-          <span class="skills_languages vuejs">VueJS</span>
-          <span class="skills_languages vitejs">ViteJS</span>
-          <span class="skills_languages php">PHP</span>
-          <span class="skills_languages java">Java</span>
-          <span class="skills_languages csharp">C#</span>
-          <span class="skills_languages visualbasic">VisualBasic</span>
-          <span class="skills_languages flutter">Flutter</span>
-          <span class="skills_languages cpp">C++</span>
+          <img title="HTML" class="skills_languages" alt="html logo in black and white" src="/html.png"/>
+          <img title="CSS" class="skills_languages" alt="css logo in black and white" src="/css.png"/>
+          <img title="Javascript" class="skills_languages" alt="js logo in black and white" src="/javascript.png"/>
+          <img title="VueJS" class="skills_languages " alt="vue js logo in black and white" src="/vuejs.png"/>
+          <img title="ViteJS" class="skills_languages" alt="vite js logo in black and white" src="/vitejs.png"/>
+          <img title="PHP" class="skills_languages" alt="php logo in black and white" src="/php-logo.png"/>
+          <img title="Java" class="skills_languages" alt="java logo in black and white" src="/java.png"/>
+          <img title="C#" class="skills_languages" alt="csharp logo in black and white" src="/c-sharp-logo.png"/>
+          <img title="Visual Basic" class="skills_languages" alt="visual basic logo in black and white" src="/VB.png"/>
+          <img title="Flutter" class="skills_languages" alt="flutter logo in black and white" src="/Flutter.png"/>
+          <img title="C++" class="skills_languages" alt="cpp logo in black and white" src="/cpp.png"/>
         </grid>
       </div>
       <div class="card-tools">
         <h1 class="title-tools">Outils</h1>
         <grid class="grid-tools">
-          <span class="skills_tools linux">Linux</span>
-          <span class="skills_tools github">GitHub</span>
-          <span class="skills_tools git">Git</span>
-          <span class="skills_tools docker">Docker</span>
-          <span class="skills_tools visualstudio">VisualStudio</span>
-          <span class="skills_tools webstorm">Webstorm</span>
-          <span class="skills_tools phpstorm">PhpStorm</span>
-          <span class="skills_tools sqlserver">SqlServer</span>
-          <span class="skills_tools mysql">MySql</span>
-          <span class="skills_tools jetbrains">Jetbrains</span>
-          <span class="skills_tools vscode">VScode</span>
+          <img title="Linux" class="skills_languages" alt="linux logo in black and white" src="/linux.png"/>
+          <img title="GitHub" class="skills_languages" alt="github logo in black and white" src="/Github-logo.webp"/>
+          <img title="GIT" class="skills_languages" alt="git logo in black and white" src="/git.png"/>
+          <img title="Docker" class="skills_languages" alt="docker logo in black and white" src="/Docker.png"/>
+          <img title="Visual Studio" class="skills_languages" alt="visual studio logo in black and white" src="/visual-studio.png"/>
+          <img title="Webstorm" class="skills_languages" alt="webstorm logo in black and white" src="/webstorm.png"/>
+          <img title="Phpstorm" class="skills_languages" alt="phpstorm logo in black and white" src="/Phpstorm.png"/>
+          <img title="SQLServer" class="skills_languages" alt="sqlserver logo in black and white" src="/sqlserver.png"/>
+          <img title="MySQL" class="skills_languages" alt="mysql logo in black and white" src="/mysql.png"/>
+          <img title="Jetbrains" class="skills_languages" alt="jetbrains logo in black and white" src="/jetbrains.jpg"/>
+          <img title="Visual Studio Code" class="skills_languages" alt="visual studio code logo in black and white" src="/vs-code.png"/>
         </grid>
       </div>
     </grid>
